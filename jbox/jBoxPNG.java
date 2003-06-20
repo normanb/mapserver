873edd216d3eae92066a13ecd74b0dc670a70fbe
greenwood <greenwood@7532c77e-422f-0410-93f4-f0b67bdd69e2>
2003-06-20 13:40:32 +0000

    cleaned up variable names renamed jBox_mouse_moved to mousemove_handler renamed jBox_distance to measure_handler added comments
    git-svn-id: http://svn.osgeo.org/mapserver/trunk@2176 7532c77e-422f-0410-93f4-f0b67bdd69e2

--- jbox/jBoxPNG.java
+++ jbox/jBoxPNG.java
@@ -7,8 +7,12 @@
 //    jitter int - minimum size (in pixels) for a dragged box.
 //    image string - url for the image to display, must be fully qualified.
 //    thickness int - thickness (in pixels) of the dragged box.
+//		cursorsize
+//		name
+//		busyimage
 //    box on/off - status for box drawing, default is on
-//    verbose on/off - turns on verbosity so that all mouse movements are passed out to javascript
+//    verbose true/false - turns on verbosity so that all mouse movements are passed out to javascript
+//			(was on/off in mapplet)
 //
 // Public methods:
 //    boxon - toggles box drawing on.
@@ -16,6 +20,13 @@
 //    setimage(string) - displays the image loaded from the passed url.
 //    setcursor(string) - sets the cursor to one of crosshair, hand or default
 //
+// javascript functions eval'd:
+//		setbox_handler
+// 	seterror_handler
+// 	reset_handler
+// 	mouseenter_handler
+// 	mouseexit_handler
+// 	mousemove_handler
 
 /*
 	Rich Greenwood, rich@greenwoodmap.com, November 2002:
@@ -25,13 +36,13 @@
 		lineon - toggles line drawing on, and box drawing off, boxoff() turns off both`
 
 	Return: javascript eval:
-		jBox_distance(name, s, t, n)
-			s = current segment length
-			t = running total length
-			n = number of vertices (for debugging)
+		measure_handler(name, seg, tot, num)
+			seg = current segment length
+			tot = running total length
+			num = number of vertices
 
 	Compiler issues:
-		Sun JDK 1.2 & 1.3 work ok,
+		Sun JDK 1.2 & 1.3 work ok, but
 		1.4 compiled classes won't run under MS VM (IE 5.5)?
 		MS J++ won't compile due to un-caught exceptions?
 */
@@ -49,7 +60,7 @@ class evalThread extends Thread {
     JSObject twindow;
     double tx1, tx2, ty1, ty2;
     String tname;
-    
+
     public evalThread(JSObject window, String name, double x1, double y1, double x2, double y2) {
 	twindow = window;
 	tname = name;
@@ -58,7 +69,7 @@ class evalThread extends Thread {
 	tx2 = x2;
 	ty2 = y2;
     }
-    
+
     public void run () {
 	twindow.eval("setbox_handler('" + tname + "'," + Math.min(tx1,tx2) + "," + Math.min(ty1,ty2) + "," + Math.max(tx1,tx2) + "," + Math.max(ty1,ty2) + ");");
     }
@@ -68,7 +79,7 @@ public class jBoxPNG extends Applet implements MouseListener, MouseMotionListene
     boolean busy=false, box=true, line=false, init=true, verbose=false;
     Image img, busyimg=null;
     PngImage pngimg;
-    double x1=-1, y1=-1, x2=-1, y2=-1, s=0, t=0;
+    double x1=-1, y1=-1, x2=-1, y2=-1, seg=0, tot=0;
     int	jitter=5, cursorsize=4, thickness=1;
     Color color=Color.red;
     JSObject window;
@@ -76,30 +87,30 @@ public class jBoxPNG extends Applet implements MouseListener, MouseMotionListene
     Graphics offScreenGraphics;
     Dimension screenSize;
     String name;
-    
+
     Polygon pl;
 
     public void init () {
 	StringTokenizer st;
-	String 	s=null, t=null;
+	String s=null;	//RWG, t=null;
 	URL url=null;
-	
+
 	screenSize = this.getSize(); // nab the applet size
-	
+
 	offScreenImage = createImage(screenSize.width, screenSize.height);
 	offScreenGraphics = offScreenImage.getGraphics();
-	
+
 	// get the Navigator window handle
 	window = JSObject.getWindow(this);
-	
+
 	s = getParameter("jitter");
 	if(s != null)
 	    jitter = Integer.parseInt(s);
-	
+
 	s = getParameter("color");
 	if(s != null)
 	    color = getColorParameter(s);
-	
+
 	s = getParameter("thickness");
 	if(s != null)
 	    thickness = Integer.parseInt(s);
@@ -107,13 +118,13 @@ public class jBoxPNG extends Applet implements MouseListener, MouseMotionListene
 	s = getParameter("cursorsize");
 	if(s != null)
 	    cursorsize = Integer.parseInt(s);
-	
+
 	s = getParameter("verbose");
 	if(s != null)
 	    if(s.equalsIgnoreCase("true")) verbose = true;
 
 	name = getParameter("name");
-	
+
 	s = getParameter("busyimage");
 	if(s != null) {
 	    try {
@@ -124,12 +135,12 @@ public class jBoxPNG extends Applet implements MouseListener, MouseMotionListene
 	    }
 	    busyimg = getImage(url);
 	}
-	
+
 	s = getParameter("box");
 	if(s != null) {
 	    if(s.equalsIgnoreCase("off")) box = false;
 	}
-	
+
 	// nab the image itself
 	s = getParameter("image");
 	try {
@@ -142,17 +153,17 @@ public class jBoxPNG extends Applet implements MouseListener, MouseMotionListene
 	} catch(java.io.IOException e) {
 	    return;
 	}
-	img = Toolkit.getDefaultToolkit().createImage(pngimg);	
-	
+	img = Toolkit.getDefaultToolkit().createImage(pngimg);
+
 	// we want mouse events and mouse movement events
 	addMouseListener(this);
 	addMouseMotionListener(this);
     }	// end of init()
-    
+
     private Color getColorParameter(String s) {
 	StringTokenizer st;
 	int r, g, b;
-	
+
 	// check if a pre-defined color is specified
 	if (s.equalsIgnoreCase("black"))
 	    return(Color.black);
@@ -180,7 +191,7 @@ public class jBoxPNG extends Applet implements MouseListener, MouseMotionListene
 	    return(Color.white);
 	if (s.equalsIgnoreCase("yellow"))
 	    return(Color.yellow);
-	
+
 	// nope, must be an RGB triplet
 	st = new StringTokenizer(s, ",");
 	r = Integer.parseInt(st.nextToken());
@@ -188,32 +199,32 @@ public class jBoxPNG extends Applet implements MouseListener, MouseMotionListene
 	b = Integer.parseInt(st.nextToken());
 	return(new Color(r,g,b));
     }	// end of getColorParameter()
-    
+
     public void setcursorimage(String s1, String s2, int x, int y) {
 	Image img;
 	URL url=null;
 	Point hotspot = new Point(x, y);
 	Toolkit tk = getToolkit();
-	
+
 	try {
 	    url = new URL(s2);
 	} catch(MalformedURLException e) {
 	    window.eval("seterror_handler('Applet error. Malformed cursor image URL.');");
 	    this.stop();
 	}
-	
+
 	img = getImage(url);
-	
+
 	try {
 	    this.setCursor(tk.createCustomCursor(img, hotspot, s1));
 	} catch(IndexOutOfBoundsException e) {
 	    window.eval("seterror_handler('Applet error. Cursor hotspot out of bounds.');");
 	    this.stop();
 	}
-	
+
 	return;
     }	// end setcursorimage()
-    
+
     public void setcursor(String name) {
 	if (name.equalsIgnoreCase("hand"))
 	    this.setCursor(java.awt.Cursor.getPredefinedCursor(java.awt.Cursor.HAND_CURSOR));
@@ -222,7 +233,7 @@ public class jBoxPNG extends Applet implements MouseListener, MouseMotionListene
 	else
 	    this.setCursor(java.awt.Cursor.getPredefinedCursor(java.awt.Cursor.DEFAULT_CURSOR));
     }
-    
+
     public void boxon () {
 	box = true;
 	line = false;
@@ -230,10 +241,10 @@ public class jBoxPNG extends Applet implements MouseListener, MouseMotionListene
 	repaint();
 	return;
     }
-    
+
     public void boxoff () {
 	int c = cursorsize;
-	
+
 	box = false;
 	line = false;
 	x2 = x1; // collapse
@@ -244,36 +255,40 @@ public class jBoxPNG extends Applet implements MouseListener, MouseMotionListene
 	window.eval("reset_handler('" + name + "'," + Math.min(x1,x2) + "," + Math.min(y1,y2) + "," + Math.max(x1,x2) + "," + Math.max(y1,y2) + ");");
 	return;
     }
-    
+
     public void lineon () {
 	box = false;
 	line = true;
 	pl = new Polygon();
-	t = s = 0;
+	tot = seg = 0;
 	return;
     }
-    
+
+	public void lineoff () {	// synonym for boxoff()
+		boxoff();
+	}
+
     public void setimage(String s) {	// was swap() in mapplet
 	URL url=null;
 	MediaTracker tracker = new MediaTracker(this);
-	
+
 	busy = true;
-	
+
 	// reinitialize the cursor position
 	x1 = x2 = (screenSize.width-1)/2.0;
 	y1 = y2 = (screenSize.height-1)/2.0;
-	
+
 	if(busyimg != null)
 	    repaint(); // show busy image
-	
+
 	// img.flush();
-	
+
 	try {
 	    url = new URL(s);
 	} catch(MalformedURLException e) {
 	    return;
 	}
-	
+
 	try {
 	    pngimg = new PngImage(url);
 	} catch(java.io.IOException e) {
@@ -281,53 +296,65 @@ public class jBoxPNG extends Applet implements MouseListener, MouseMotionListene
 	}
 	img = Toolkit.getDefaultToolkit().createImage(pngimg);
 	tracker.addImage(img, 0);
-	
+
 	try {
 	    tracker.waitForID(0); // wait till it loads
 	} catch (InterruptedException e) {
 	    return;
 	}
-	
+
 	busy = false;
 	repaint();
 	window.eval("reset_handler('" + name + "'," + Math.min(x1,x2) + "," + Math.min(y1,y2) + "," + Math.max(x1,x2) + "," + Math.max(y1,y2) + ");");
-	
+
 	return;
     }	// end setimage()
-    
+
     //
     // Mouse event handlers
     //
-    public void mouseClicked(MouseEvent event) {	}
-    
+	public void mouseClicked(MouseEvent event) {
+		// look for double click; reset line
+		if (event.getClickCount() >= 2)	{
+			if (line) {
+				pl = new Polygon();
+				// forcing x1 and x2 to be different prevents this double click
+				// from starting a new pline / measure sequence by causing it
+				// to fail the mouse click test in paint()
+				x1=0;
+				x2=1;
+			}
+		}
+	}
+
     public void mouseEntered(MouseEvent event) {
 	if(verbose)	window.eval("mouseenter_handler('" + this.name + "');");
     }
-    
+
     public void mouseExited(MouseEvent event) {
 	if(verbose) window.eval("mouseexit_handler('" + this.name + "');");
     }
-    
+
     public void mouseMoved(MouseEvent event) {
 	if(verbose)
-	    window.eval("javascript:jBox_mouse_moved('" + this.name + "'," + event.getX() + "," + event.getY() + ");");
+	    window.eval("mousemove_handler('" + this.name + "'," + event.getX() + "," + event.getY() + ");");
 	if(line) {
 	    x2 = event.getX();
 	    y2 = event.getY();
 	    repaint();
 	}
     }
-    
+
     public void mousePressed(MouseEvent event) {
 	x1 = x2 = event.getX();
 	y1 = y2 = event.getY();
 	if (line) repaint();
     }
-    
+
     public void mouseDragged(MouseEvent event) {
 	if(verbose)
-	    window.eval("javascript:jBox_mouse_moved('" + this.name + "'," + event.getX() + "," + event.getY() + ");");
-	
+	    window.eval("mousemove_handler('" + this.name + "'," + event.getX() + "," + event.getY() + ");");
+
 	x2 = event.getX();
 	y2 = event.getY();
 	if(!box && !line) {
@@ -336,7 +363,7 @@ public class jBoxPNG extends Applet implements MouseListener, MouseMotionListene
 	}
 	repaint();
     }
-    
+
     public void mouseReleased(MouseEvent event) {
 	// window.eval("javascript:jBox_debug_print('mouseReleased');");
 	if(box || line) {
@@ -346,10 +373,10 @@ public class jBoxPNG extends Applet implements MouseListener, MouseMotionListene
 	    if ( x2 < 0 ) { x2 = 0; }
 	    if ( y2 > screenSize.height) { y2 = screenSize.height-1; }
 	    if ( y2 < 0 ) { y2 = 0; }
-	    
+
 	    // check to see if (x2,y2) forms a large enough rectangle
 	    // to be considered a new extent or if the user is just a
-	    // poor mouse clikcker
+	    // poor mouse clicker
 	    if((Math.abs(x1-x2) <= jitter) || (Math.abs(y1-y2) <= jitter)) {
 		x2 = x1;
 		y2 = y1;
@@ -358,49 +385,47 @@ public class jBoxPNG extends Applet implements MouseListener, MouseMotionListene
 	    x2 = x1;
 	    y2 = y1;
 	}
-	
+
 	repaint();
-	
+
 	// this a time for a re-draw if the application so chooses
 	if(!busy && !line)	// don't want a form-submit in line drawing mode
 	    new evalThread(window, name, x1, y1, x2, y2).start();
     }	// mouseReleased
-    
+
     public void paint(Graphics g) {
 	int x, y, w, h, i;
-	
+
 	// draw the image
 	offScreenGraphics.drawImage(img,0,0,this);
-	
-	// draw the user defined rectangle or crosshair
+
+	// draw the user defined pline, rectangle or crosshair
 	offScreenGraphics.setColor(color);
-	
+
 	if (line) {
-	    if ((x1==x2) && (y1==y2)) {	// mouse click
-		if (pl.npoints == 0) {	// don't question first point
-		    pl.addPoint((int)x2, (int)y2);
-		    
-		} else if (! ((x1 == pl.xpoints[pl.npoints-1]) && (y1 == pl.ypoints[pl.npoints-1]))) {
-		    pl.addPoint((int)x2, (int)y2);
-		    t+=s;
+		if ((x1==x2) && (y1==y2)) {	// mouse click
+			if (pl.npoints == 0) {	// don't question first point
+				pl.addPoint((int)x2, (int)y2);
+				tot=0;
+			} else if (! ((x1 == pl.xpoints[pl.npoints-1]) && (y1 == pl.ypoints[pl.npoints-1]))) {
+				pl.addPoint((int)x2, (int)y2);
+				tot+=seg;
+			}
 		}
-	    }
-	    
-	    offScreenGraphics.drawPolyline(pl.xpoints, pl.ypoints, pl.npoints);
-	    
-	    if (pl.npoints > 0) {	// don't rubberband w/o starting point
-		offScreenGraphics.drawLine((int)x1, (int)y1, (int)x2, (int)y2);
-		s = Math.sqrt(Math.pow(y2 - pl.ypoints[pl.npoints-1],2) + Math.pow(x2 - pl.xpoints[pl.npoints-1],2));
-	    }
-	    
-	    window.eval("javascript:jBox_distance('" + name + "'," + s + "," + t +"," + pl.npoints + ");");
-	    
+
+		if (pl.npoints > 0) {	// don't do anything w/o starting point
+			offScreenGraphics.drawPolyline(pl.xpoints, pl.ypoints, pl.npoints);
+			offScreenGraphics.drawLine((int)x1, (int)y1, (int)x2, (int)y2);
+			seg = Math.sqrt(Math.pow(y2 - pl.ypoints[pl.npoints-1],2) + Math.pow(x2 - pl.xpoints[pl.npoints-1],2));
+			window.eval("measure_handler('" + name + "'," + seg + "," + tot +"," + pl.npoints + ");");
+		}
+
 	} else if (box) {
 	    x = (int)Math.min(x1,x2);
 	    y = (int)Math.min(y1,y2);
 	    w = (int)Math.abs(x1-x2);
 	    h = (int)Math.abs(y1-y2);
-	    
+
 	    for(i=0; i<thickness; i++)
 		offScreenGraphics.drawRect(x+i, y+i, w-(2*i), h-(2*i));
 	} else if((x1==x2) && (y1==y2)) {
@@ -409,18 +434,18 @@ public class jBoxPNG extends Applet implements MouseListener, MouseMotionListene
 		offScreenGraphics.drawLine((int)x2, (int)(y2-cursorsize), (int)x2, (int)(y2+cursorsize));
 	    }
 	}
-	
+
 	if(busy && busyimg != null) {
 	    x = screenSize.width/2 - busyimg.getWidth(this)/2;
 	    y = screenSize.height/2 - busyimg.getHeight(this)/2;
 	    offScreenGraphics.drawImage(busyimg,x,y,this);
 	}
-	
+
 	g.drawImage(offScreenImage, 0, 0, this);
     }	// end of paint
-    
+
     public void destroy () {}
-    
+
     public void update (Graphics g) {
 	paint(g);
     }
