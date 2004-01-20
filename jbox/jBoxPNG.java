68a6d585b19c904839f31a6d884c65c3910b2709
greenwood <greenwood@7532c77e-422f-0410-93f4-f0b67bdd69e2>
2004-01-20 23:12:47 +0000

    Rich Greenwood rich@greenwoodmap.com put evalThread into a seperated file to avoid Ant build errors
    git-svn-id: http://svn.osgeo.org/mapserver/trunk@2636 7532c77e-422f-0410-93f4-f0b67bdd69e2

--- jbox/jBoxPNG.java
+++ jbox/jBoxPNG.java
@@ -51,25 +51,6 @@ import java.util.*;
 import netscape.javascript.*;
 import com.sixlegs.image.png.*;
 
-class evalThread extends Thread {
-    JSObject twindow;
-    double tx1, tx2, ty1, ty2;
-    String tname;
-
-    public evalThread(JSObject window, String name, double x1, double y1, double x2, double y2) {
-		twindow = window;
-		tname = name;
-		tx1 = x1;
-		ty1 = y1;
-		tx2 = x2;
-		ty2 = y2;
-    }
-
-    public void run () {
-	twindow.eval("setbox_handler('" + tname + "'," + Math.min(tx1,tx2) + "," + Math.min(ty1,ty2) + "," + Math.max(tx1,tx2) + "," + Math.max(ty1,ty2) + ");");
-    }
-}	// end of class evalThread
-
 public class jBoxPNG extends Applet implements MouseListener, MouseMotionListener {
 	boolean	busy=false, box=true, line=false, drag=false, init=true, verbose=false;
     Image img, busyimg=null;
