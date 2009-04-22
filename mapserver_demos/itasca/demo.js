ea9a7f937efc089df574c84c3d89a584227d3b2f
sdlime <sdlime@7532c77e-422f-0410-93f4-f0b67bdd69e2>
2009-04-22 04:31:55 +0000

    Updating...
    git-svn-id: http://svn.osgeo.org/mapserver/trunk@8938 7532c77e-422f-0410-93f4-f0b67bdd69e2

---
+++ mapserver_demos/itasca/demo.js
@@ -0,0 +1,11 @@
+//
+// Edit the mapserv variable below to match your system. Typically Unix/Linux users will
+// set it to /cgi-bin/mapserv and Windows users will set it to /cgi-bin/mapserv.exe.
+//
+var mapserv = '/cgi-bin/mapserv';
+
+//
+// Edit the mapfile variable below to match your system. It should reflect the full path
+// to the demo.map file supplied with the demo.
+//
+var mapfile = '../demo.map';
