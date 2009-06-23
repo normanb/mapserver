normanb/mapserver

commit cd4a054154d01df3f276c7996273e24d9a54f52f

refactor renderer plugins to follow the layer vtable approach a bit more closely
git-svn-id: http://svn.osgeo.org/mapserver/trunk@9114 7532c77e-422f-0410-93f4-f0b67bdd69e2

mapserver/mapoutput.c
@@ -228,7 +228,6 @@ outputFormatObj *msCreateDefaultOutputFormat( mapObj *map,
         format->imagemode = MS_IMAGEMODE_RGB;
         format->extension = strdup("png");
         format->renderer = MS_RENDER_WITH_CAIRO_RASTER;
-        format->r = msCreateRenderer(MS_RENDER_WITH_CAIRO_RASTER);
     }
     if( strcasecmp(driver,"CAIRO/JPEG") == 0 )
     {
@@ -237,7 +236,6 @@ outputFormatObj *msCreateDefaultOutputFormat( mapObj *map,
         format->imagemode = MS_IMAGEMODE_RGB;
         format->extension = strdup("jpg");
         format->renderer = MS_RENDER_WITH_CAIRO_RASTER;
-        format->r = msCreateRenderer(MS_RENDER_WITH_CAIRO_RASTER);
     }
     if( strcasecmp(driver,"CAIRO/PDF") == 0 )
 	{
@@ -245,8 +243,7 @@ outputFormatObj *msCreateDefaultOutputFormat( mapObj *map,
 		format->mimetype = strdup("application/x-pdf");
 		format->imagemode = MS_IMAGEMODE_RGB;
 		format->extension = strdup("pdf");
-		format->renderer = MS_RENDER_WITH_CAIRO_VECTOR;
-		format->r = msCreateRenderer(MS_RENDER_WITH_CAIRO_VECTOR);
+		format->renderer = MS_RENDER_WITH_CAIRO_PDF;
 	}
     if( strcasecmp(driver,"CAIRO/SVG") == 0 )
 	{
@@ -254,8 +251,7 @@ outputFormatObj *msCreateDefaultOutputFormat( mapObj *map,
 		format->mimetype = strdup("image/svg+xml");
 		format->imagemode = MS_IMAGEMODE_RGB;
 		format->extension = strdup("svg");
-		format->renderer = MS_RENDER_WITH_CAIRO_VECTOR;
-		format->r = msCreateRenderer(MS_RENDER_WITH_CAIRO_VECTOR);
+		format->renderer = MS_RENDER_WITH_CAIRO_SVG;
 	}
     
     
@@ -269,7 +265,6 @@ outputFormatObj *msCreateDefaultOutputFormat( mapObj *map,
         format->imagemode = MS_IMAGEMODE_RGB; 
         format->extension = strdup("png"); 
         format->renderer = MS_RENDER_WITH_OGL; 
-        format->r = msCreateRenderer(MS_RENDER_WITH_OGL); 
     } 
 #endif 
  
@@ -339,7 +334,10 @@ outputFormatObj *msCreateDefaultOutputFormat( mapObj *map,
 
     if( format != NULL )
       format->inmapfile = MS_FALSE;
-
+    
+    if( format!= NULL && MS_RENDERER_PLUGIN(format) ) {
+        msInitializeRendererVTable(format);
+    }
     return format;
 }
 
@@ -437,7 +435,8 @@ void msFreeOutputFormat( outputFormatObj *format )
     msFree( format->driver );
     msFree( format->extension );
     msFreeCharArray( format->formatoptions, format->numformatoptions );
-    msFree( format->r);
+    /* msFreeRendererVTable( format->vtable ); */
+    msFree( format->vtable );
     msFree( format );
 }
 
@@ -468,7 +467,7 @@ static outputFormatObj *msAllocOutputFormat( mapObj *map, const char *name,
     format->name = strdup(name);
     format->driver = strdup(driver);
     format->refcount = 0;
-    format->r = NULL;
+    format->vtable = NULL;
     format->imagemode = MS_IMAGEMODE_PC256;
 
 /* -------------------------------------------------------------------- */
@@ -752,7 +751,6 @@ outputFormatObj *msCloneOutputFormat( outputFormatObj *src )
 
     dst->imagemode = src->imagemode;
     dst->renderer = src->renderer;
-    dst->r = src->r;
     
     dst->transparent = src->transparent;
     dst->bands = src->bands;
@@ -766,6 +764,8 @@ outputFormatObj *msCloneOutputFormat( outputFormatObj *src )
 
     dst->inmapfile = src->inmapfile;
 
+    msInitializeRendererVTable(dst);
+    
     return dst;
 }
 
@@ -1002,93 +1002,30 @@ int msOutputFormatValidate( outputFormatObj *format )
     return result;
 }
 
-renderObj* msCreateRenderer(int type) {
-    renderObj *r = malloc(sizeof(renderObj));
-    switch(type) {
-#ifdef USE_CAIRO
-    case MS_RENDER_WITH_CAIRO_RASTER:
-        r->supports_imagecache=0;
-        r->supports_pixel_buffer=1;
-        r->supports_transparent_layers = 1;
-        r->startNewLayer = startNewLayerCairo;
-        r->closeNewLayer = closeNewLayerCairo;
-        r->renderLine=&renderLineCairo;
-        r->createImage=&createImageCairo;
-        r->saveImage=&saveImageCairo;
-        r->getRasterBuffer=&getRasterBufferCairo;
-        r->transformShape=&msTransformShapeAGG;
-        r->renderPolygon=&renderPolygonCairo;
-        r->renderGlyphsLine=&renderGlyphsLineCairo;
-        r->renderGlyphs=&renderGlyphsCairo;
-        r->freeImage=&freeImageCairo;
-        r->renderEllipseSymbol = &renderEllipseSymbolCairo;
-        r->renderVectorSymbol = &renderVectorSymbolCairo;
-        r->renderTruetypeSymbol = &renderTruetypeSymbolCairo;
-        r->renderPixmapSymbol = &renderPixmapSymbolCairo;
-        r->mergeRasterBuffer = &mergeRasterBufferCairo;
-        r->getTruetypeTextBBox = &getTruetypeTextBBoxCairo;
-        r->renderTile = &renderTileCairo;
-        r->renderPolygonTiled = &renderPolygonTiledCairo;
-        r->freeTile = &freeTileCairo;
-        r->freeSymbol = &freeSymbolCairo;
-        return r;
-    case MS_RENDER_WITH_CAIRO_VECTOR:
-        r->supports_imagecache=0;
-        r->supports_pixel_buffer=0;
-        r->supports_transparent_layers = 1;
-    	r->startNewLayer = startNewLayerCairo;
-    	r->closeNewLayer = closeNewLayerCairo;
-        r->renderLine=&renderLineCairo;
-        r->createImage=&createImageCairo;
-        r->saveImage=&saveImageCairo;
-        r->getRasterBuffer=&getRasterBufferCairo;
-        r->transformShape=&msTransformShapeAGG;
-        r->renderPolygon=&renderPolygonCairo;
-        r->renderGlyphsLine=&renderGlyphsLineCairo;
-        r->renderGlyphs=&renderGlyphsCairo;
-        r->freeImage=&freeImageCairo;
-        r->renderEllipseSymbol = &renderEllipseSymbolCairo;
-        r->renderVectorSymbol = &renderVectorSymbolCairo;
-        r->renderTruetypeSymbol = &renderTruetypeSymbolCairo;
-        r->renderPixmapSymbol = &renderPixmapSymbolCairo;
-        r->mergeRasterBuffer = &mergeRasterBufferCairo;
-        r->getTruetypeTextBBox = &getTruetypeTextBBoxCairo;
-        r->renderTile = &renderTileCairo;
-        r->renderPolygonTiled = &renderPolygonTiledCairo;
-        r->freeTile = &freeTileCairo;
-        r->freeSymbol = &freeSymbolCairo;
-        return r;
-#endif
-#ifdef USE_OGL
-    case MS_RENDER_WITH_OGL:
-    	r->supports_transparent_layers = 1;
-    	r->startNewLayer = msStartNewLayerOgl;
-    	r->closeNewLayer = msCloseNewLayerOgl;
-        r->renderLine=&msDrawLineOgl;
-        r->createImage=&msImageCreateOgl;
-        r->saveImage=&msSaveImageOgl;
-        r->transformShape=&msTransformShapeAGG;
-        r->renderPolygon=&msDrawPolygonOgl;
-        r->renderGlyphs=&msRenderGlyphsOgl;
-        r->renderEllipseSymbol = &msRenderEllipseOgl;
-        r->renderVectorSymbol = &msRenderVectorSymbolOgl;
-        r->renderPixmapSymbol = &msRenderPixmapOgl;
-        r->mergeRasterBuffer = &msMergeImagesOgl;
-        r->getTruetypeTextBBox = &msGetTruetypeTextBBoxOgl;
-        r->createPixmapSymbolTile = &msCreateTilePixmapOgl;
-        r->createVectorSymbolTile = &msCreateTileVectorOgl;
-        r->createEllipseSymbolTile = &msCreateTileEllipseOgl;
-        r->createTruetypeSymbolTile = &msCreateTileTruetypeOgl;
-        r->renderTile = &msRenderTileOgl;
-        r->renderPolygonTiled = &msDrawPolygonTiledOgl;
-        r->renderLineTiled = &msDrawLineTiledOgl;
-        r->freeTile = &msFreeTileOgl;
-        r->freeSymbol = &msFreeSymbolOgl;
-        r->freeImage=&msFreeImageOgl;
-        return r;
-#endif
-
-    default:
-        return NULL;
+int msInitializeRendererVTable(outputFormatObj *format) {
+    assert(format);
+    if(format->vtable) {
+        /* TODO?: clean up caches before switch 
+        msFreeRendererVTable(format->vtable); */
+        msFree(format->vtable);
     }
+    format->vtable = (rendererVTableObj*)malloc(sizeof(rendererVTableObj));
+    
+    switch(format->renderer) {
+        case MS_RENDER_WITH_CAIRO_RASTER:
+            return msPopulateRendererVTableCairoRaster(format->vtable);
+        case MS_RENDER_WITH_CAIRO_PDF:
+            return msPopulateRendererVTableCairoPDF(format->vtable);
+        case MS_RENDER_WITH_CAIRO_SVG:
+            return msPopulateRendererVTableCairoSVG(format->vtable);
+        case MS_RENDER_WITH_OGL:
+            return msPopulateRendererVTableOGL(format->vtable);
+        default:
+            msSetError(MS_MISCERR, "unsupported RendererVtable renderer %d",
+                    "msInitializeRendererVTable()",format->renderer);
+            return MS_FAILURE;            
+    }
+    /* this code should never be executed */
+    return MS_FAILURE;
 }
+

mapserver/mapcairo.c
@@ -26,10 +26,10 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
  *****************************************************************************/
+#include "mapserver.h"
 
 #ifdef USE_CAIRO
 
-#include "mapserver.h"
 #include <cairo.h>
 #if defined(_WIN32) && !defined(__CYGWIN__)
 #include <cairo-pdf.h>
@@ -49,6 +49,8 @@
 #include <ft2build.h>
 #include FT_FREETYPE_H
 */
+
+
 
 
 /* trivial cache container */
@@ -749,4 +751,81 @@ void freeSymbolCairo(symbolObj *s) {
 	s->renderer_cache=NULL;
 }
 
+#endif /*USE_CAIRO*/
+
+
+int msPopulateRendererVTableCairoRaster( rendererVTableObj *renderer ) {
+#ifdef USE_CAIRO
+    renderer->supports_imagecache=0;
+    renderer->supports_pixel_buffer=1;
+    renderer->supports_transparent_layers = 1;
+    renderer->startNewLayer = startNewLayerCairo;
+    renderer->closeNewLayer = closeNewLayerCairo;
+    renderer->renderLine=&renderLineCairo;
+    renderer->createImage=&createImageCairo;
+    renderer->saveImage=&saveImageCairo;
+    renderer->getRasterBuffer=&getRasterBufferCairo;
+    renderer->transformShape=&msTransformShapeAGG;
+    renderer->renderPolygon=&renderPolygonCairo;
+    renderer->renderGlyphsLine=&renderGlyphsLineCairo;
+    renderer->renderGlyphs=&renderGlyphsCairo;
+    renderer->freeImage=&freeImageCairo;
+    renderer->renderEllipseSymbol = &renderEllipseSymbolCairo;
+    renderer->renderVectorSymbol = &renderVectorSymbolCairo;
+    renderer->renderTruetypeSymbol = &renderTruetypeSymbolCairo;
+    renderer->renderPixmapSymbol = &renderPixmapSymbolCairo;
+    renderer->mergeRasterBuffer = &mergeRasterBufferCairo;
+    renderer->getTruetypeTextBBox = &getTruetypeTextBBoxCairo;
+    renderer->renderTile = &renderTileCairo;
+    renderer->renderPolygonTiled = &renderPolygonTiledCairo;
+    renderer->freeTile = &freeTileCairo;
+    renderer->freeSymbol = &freeSymbolCairo;
+    return MS_SUCCESS;
+#else
+    msSetError(MS_MISCERR, "Cairo Driver requested but is not built in", 
+            "msPopulateRendererVTableCairoRaster()");
+    return MS_FAILURE;
 #endif
+}
+
+inline int populateRendererVTableCairoVector( rendererVTableObj *renderer ) {
+#ifdef USE_CAIRO
+    renderer->supports_imagecache=0;
+    renderer->supports_pixel_buffer=0;
+    renderer->supports_transparent_layers = 1;
+    renderer->startNewLayer = startNewLayerCairo;
+    renderer->closeNewLayer = closeNewLayerCairo;
+    renderer->renderLine=&renderLineCairo;
+    renderer->createImage=&createImageCairo;
+    renderer->saveImage=&saveImageCairo;
+    renderer->getRasterBuffer=&getRasterBufferCairo;
+    renderer->transformShape=&msTransformShapeAGG;
+    renderer->renderPolygon=&renderPolygonCairo;
+    renderer->renderGlyphsLine=&renderGlyphsLineCairo;
+    renderer->renderGlyphs=&renderGlyphsCairo;
+    renderer->freeImage=&freeImageCairo;
+    renderer->renderEllipseSymbol = &renderEllipseSymbolCairo;
+    renderer->renderVectorSymbol = &renderVectorSymbolCairo;
+    renderer->renderTruetypeSymbol = &renderTruetypeSymbolCairo;
+    renderer->renderPixmapSymbol = &renderPixmapSymbolCairo;
+    renderer->mergeRasterBuffer = &mergeRasterBufferCairo;
+    renderer->getTruetypeTextBBox = &getTruetypeTextBBoxCairo;
+    renderer->renderTile = &renderTileCairo;
+    renderer->renderPolygonTiled = &renderPolygonTiledCairo;
+    renderer->freeTile = &freeTileCairo;
+    renderer->freeSymbol = &freeSymbolCairo;
+    return MS_SUCCESS;
+#else
+    msSetError(MS_MISCERR, "Cairo Driver requested but is not built in", 
+            "msPopulateRendererVTableCairoRaster()");
+    return MS_FAILURE;
+#endif
+}
+
+int msPopulateRendererVTableCairoSVG( rendererVTableObj *renderer ) {
+    return populateRendererVTableCairoVector(renderer);
+}
+int msPopulateRendererVTableCairoPDF( rendererVTableObj *renderer ) {
+    return populateRendererVTableCairoVector(renderer);
+}
+

mapserver/mapdraw.c
@@ -148,8 +148,8 @@ imageObj *msPrepareImage(mapObj *map, int allow_nonsquare)
         return(NULL);
     }
     else if (MS_RENDERER_PLUGIN(map->outputformat)) {
-		renderObj *r = map->outputformat->r;
-		image = r->createImage(map->width, map->height, map->outputformat,&map->imagecolor);
+		rendererVTableObj *renderer = map->outputformat->vtable;
+		image = renderer->createImage(map->width, map->height, map->outputformat,&map->imagecolor);
         if (image == NULL)
             return(NULL);
 		image->format = map->outputformat;
@@ -745,7 +745,7 @@ int msDrawLayer(mapObj *map, layerObj *layer, imageObj *image)
   }
   else if (MS_RENDERER_PLUGIN(image_draw->format)) {
 		if (layer->opacity > 0 && layer->opacity < 100) {
-			if (!image_draw->format->r->supports_transparent_layers) {
+			if (!image_draw->format->vtable->supports_transparent_layers) {
 				msApplyOutputFormat(&transFormat, image->format, MS_TRUE,
 						MS_NOOVERRIDE,MS_NOOVERRIDE);
 				image_draw = msImageCreate(image->width, image->height,
@@ -756,7 +756,7 @@ int msDrawLayer(mapObj *map, layerObj *layer, imageObj *image)
 					return (MS_FAILURE);
 				}
 			} else {
-				image_draw->format->r->startNewLayer(image_draw,layer->opacity);
+				image_draw->format->vtable->startNewLayer(image_draw,layer->opacity);
 			}
 		} 
   }
@@ -826,16 +826,17 @@ int msDrawLayer(mapObj *map, layerObj *layer, imageObj *image)
     msApplyOutputFormat( &transFormat, NULL, MS_NOOVERRIDE, MS_NOOVERRIDE, MS_NOOVERRIDE );
   }
   else if( MS_RENDERER_PLUGIN(image_draw->format) && layer->opacity > 0 && layer->opacity < 100 ) {
-	  if (!image_draw->format->r->supports_transparent_layers) {
+	  rendererVTableObj *renderer = image_draw->format->vtable;
+      if (!renderer->supports_transparent_layers) {
           rasterBufferObj rb;
-          image_draw->format->r->getRasterBuffer(image_draw,&rb);
-		  image_draw->format->r->mergeRasterBuffer(image,&rb,layer->opacity*0.01,0,0);  
-		  image_draw->format->r->freeImage( image_draw );
+          renderer->getRasterBuffer(image_draw,&rb);
+		  renderer->mergeRasterBuffer(image,&rb,layer->opacity*0.01,0,0);  
+		  renderer->freeImage( image_draw );
 	
 		  /* deref and possibly free temporary transparent output format.  */
 		  msApplyOutputFormat( &transFormat, NULL, MS_NOOVERRIDE, MS_NOOVERRIDE, MS_NOOVERRIDE );
 	  } else {
-		  image_draw->format->r->closeNewLayer(image_draw,layer->opacity*0.01);
+		  renderer->closeNewLayer(image_draw,layer->opacity*0.01);
 	  }
   }
 #ifdef USE_AGG

mapserver/maplabel.c
@@ -666,7 +666,7 @@ int msLoadFontSet(fontSetObj *fontset, mapObj *map)
 int msGetTruetypeTextBBox(imageObj *img, char *font, double size, char *string, rectObj *rect, double **advances) {
 #ifdef USE_GD_FT
 	if(img!=NULL && MS_RENDERER_PLUGIN(img->format)) {
-		img->format->r->getTruetypeTextBBox(img,font,size,string,rect,advances);
+		img->format->vtable->getTruetypeTextBBox(img,font,size,string,rect,advances);
 		//printf("%s: %f %f %f %f\n",string,rect->minx,rect->miny,rect->maxx,rect->maxy);
 		return MS_SUCCESS;
 	} else 

mapserver/mapogl.cpp
@@ -5,10 +5,10 @@
  *      Author: toby
  */
 
-#ifdef USE_OGL
-#include <assert.h>
 #include "mapserver.h"
 
+#ifdef USE_OGL
+#include <assert.h>
 #include "mapoglrenderer.h"
 #include "mapoglcontext.h"
 
@@ -215,5 +215,40 @@ void msFreeSymbolOgl(symbolObj *s)
 {
 	
 }
-
 #endif /* USE_OGL */
+
+int msPopulateRendererVTableOGL(rendererVTableObj *renderer) {
+#ifdef USE_OGL
+    	renderer->supports_transparent_layers = 1;
+    	renderer->startNewLayer = msStartNewLayerOgl;
+    	renderer->closeNewLayer = msCloseNewLayerOgl;
+        renderer->renderLine=&msDrawLineOgl;
+        renderer->createImage=&msImageCreateOgl;
+        renderer->saveImage=&msSaveImageOgl;
+        renderer->transformShape=&msTransformShapeAGG;
+        renderer->renderPolygon=&msDrawPolygonOgl;
+        renderer->renderGlyphs=&msRenderGlyphsOgl;
+        renderer->renderEllipseSymbol = &msRenderEllipseOgl;
+        renderer->renderVectorSymbol = &msRenderVectorSymbolOgl;
+        renderer->renderPixmapSymbol = &msRenderPixmapOgl;
+        renderer->mergeRasterBuffer = &msMergeImagesOgl;
+        renderer->getTruetypeTextBBox = &msGetTruetypeTextBBoxOgl;
+        renderer->createPixmapSymbolTile = &msCreateTilePixmapOgl;
+        renderer->createVectorSymbolTile = &msCreateTileVectorOgl;
+        renderer->createEllipseSymbolTile = &msCreateTileEllipseOgl;
+        renderer->createTruetypeSymbolTile = &msCreateTileTruetypeOgl;
+        renderer->renderTile = &msRenderTileOgl;
+        renderer->renderPolygonTiled = &msDrawPolygonTiledOgl;
+        renderer->renderLineTiled = &msDrawLineTiledOgl;
+        renderer->freeTile = &msFreeTileOgl;
+        renderer->freeSymbol = &msFreeSymbolOgl;
+        renderer->freeImage=&msFreeImageOgl;
+        return MS_SUCCESS;
+    #else
+        msSetError(MS_MISCERR,"OGL driver requested but it is not compiled in this release",
+                "msPopulateRendererVTableOGL()");
+        return MS_FAILURE;
+#endif
+
+}
+

commit 21db1410c0fb3240b22b7bb4e64a67666089cd7f

Fix for the problem introduced with r9114
git-svn-id: http://svn.osgeo.org/mapserver/trunk@9117 7532c77e-422f-0410-93f4-f0b67bdd69e2

mapserver/mapoutput.c
@@ -764,7 +764,8 @@ outputFormatObj *msCloneOutputFormat( outputFormatObj *src )
 
     dst->inmapfile = src->inmapfile;
 
-    msInitializeRendererVTable(dst);
+    if( MS_RENDERER_PLUGIN(dst) ) {
+        msInitializeRendererVTable(dst);
     
     return dst;
 }