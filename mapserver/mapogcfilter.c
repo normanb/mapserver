normanb/mapserver

commit 1e40994f99e821fbfd050cc131508de98fdd1884

check for inversed axis in ogc filter bbox request
git-svn-id: http://svn.osgeo.org/mapserver/trunk@10459 7532c77e-422f-0410-93f4-f0b67bdd69e2

mapserver/mapogcfilter.c
@@ -3983,6 +3983,16 @@ int FLTParseGMLEnvelope(CPLXMLNode *psRoot, rectObj *psBbox, char **ppszSRS)
             }
         }
     }
+    if (bValid && ppszSRS)
+    {
+        projectionObj sProjTmp;
+        msInitProjection(&sProjTmp);
+        if (msLoadProjectionStringEPSG(&sProjTmp, *ppszSRS) == 0)
+        {
+            msAxisNormalizePoints( &sProjTmp, 1, &psBbox->minx, &psBbox->miny);
+            msAxisNormalizePoints( &sProjTmp, 1, &psBbox->maxx, &psBbox->maxy);
+        }
+    }
     return bValid;
 }
 

mapserver/mapwfs.c
@@ -159,7 +159,17 @@ int msWFSLocateSRSInList(const char *pszList, const char *srs)
     if (strncasecmp(srs, "EPSG:",5) == 0)
       code = srs+5;
     else if (strncasecmp(srs, "urn:ogc:def:crs:EPSG:",21) == 0)
-      code = srs+21;
+    {
+        if (srs[21] == ':')
+          code = srs+21;
+        else
+          code = srs+20;
+
+         while( *code != ':' && *code != '\0')
+           code++;
+         if( *code == ':' )
+           code++;
+    }
     else if (strncasecmp(srs, "urn:EPSG:geographicCRS:",23) == 0)
       code = srs + 23;
     else

commit 5a6c6f917ab1b6873ee2e1fad8f18f5719ba753f

Fix for the problem introduced with r10459 causing a crash in msautotest
git-svn-id: http://svn.osgeo.org/mapserver/trunk@10464 7532c77e-422f-0410-93f4-f0b67bdd69e2

mapserver/mapogcfilter.c
@@ -3984,7 +3984,7 @@ int FLTParseGMLEnvelope(CPLXMLNode *psRoot, rectObj *psBbox, char **ppszSRS)
             }
         }
     }
-    if (bValid && ppszSRS)
+    if (bValid && ppszSRS && *ppszSRS)
     {
         projectionObj sProjTmp;
         msInitProjection(&sProjTmp);